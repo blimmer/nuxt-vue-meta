1801f5245a9fe60d56f1cc2d0392980c09b88f23
Ben Limmer <ben@benlimmer.com>
2023-06-12 14:06:28 -0600

    test vue meta

---
+++ pages/3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtLink to="/">/</NuxtLink>
+    <NuxtLink to="1">1</NuxtLink>
+    <NuxtLink to="2">2</NuxtLink>
+    <NuxtLink to="3">3</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: '3',
+  head() {
+    return {
+      title: '3'
+    }
+  }
+})
+</script>
